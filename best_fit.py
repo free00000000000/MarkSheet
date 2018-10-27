0414f1bb3d001ad7bd6cbd2c9dfd324bbeb21a6b
QAQ <free00000000000@gmail.com>
2018-10-27 20:15:13 +0800

    分辨出樂譜和文字

--- best_fit.py
+++ best_fit.py
@@ -2,13 +2,12 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def partition(img, threshold=0):
+def partition(img_gray, threshold=0):
     print("Partitioning...")
     import time
     start_time = time.time()
 
     weigh = img.shape[1]
-    ret, img_gray = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
     
     row = np.dot(img_gray, np.ones(weigh, np.int32))
     x = [i for i in range(row.size) if row[i] <= threshold]
@@ -19,7 +18,15 @@ def partition(img, threshold=0):
                 cut[-1][1] = x[i+1]
             else:
                 cut.append([x[i], x[i+1]])
-
+    
+    hold = weigh*(2/3)*255
+    for c in range(len(cut)):
+        s = np.max(row[cut[c][0]:cut[c][1]])
+        if s > hold:  # 是五線譜
+            cut[c].append(1)
+        else:
+            cut[c].append(0)
+    
     print("--- %s seconds ---" % (time.time() - start_time))
 
     # # draw
@@ -28,13 +35,11 @@ def partition(img, threshold=0):
     #     for j in i:
     #         cv2.line(img, (0, j), (weigh, j), (150, 255, 255), 2)
     # cv2.imwrite('./output/partition.png', img)
-
     return cut
 
 if __name__ == "__main__":
-    img_file = "./resources/samples/sheet.jpg"
-    staff_file = "./resources/template/staff.png"
-    # img_file = "../SheetVision/resources/samples/races.png"
+    img_file = "./resources/sheet.jpg"
 
     img = cv2.imread(img_file, 0)
-    partition(img)
+    ret, img_gray = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+    partition(img_gray)
